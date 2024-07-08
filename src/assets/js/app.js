6da8e5fd5acec30a615acb4611fd1289c014c0e9
hassanosamaa <almsryh877@gmail.com>
2024-07-09 02:20:48 +0300

    o

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,14 +1,6 @@
 import $ from 'jquery';
 import 'bootstrap';
 import "slick-carousel"
-import gsap from 'gsap';
-import {sec1} from "./sec1";
-import {sec3} from "./sec3";
-import {sec6} from "./sec6";
-import {sec8} from "./sec8";
-import {sec9} from "./sec9";
-import {sec11} from "./sec11";
-import {nav} from "./nav";
 
 
 $(document).ready(() => {
